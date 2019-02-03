d5f303d7b9816406a97cf40d8d1dd27e7634046a
Richard Kille <rjkille@gmail.com>
2019-02-03 20:14:35 +0000

    rerouted handler

--- handlers.go
+++ handlers.go
@@ -10,12 +10,16 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	index, err := ioutil.ReadFile("public/index.html")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Failed to read index: "+err.Error())
+		return
 	}
 
 	_, err = fmt.Fprint(w, string(index))
 
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, "Faiiled to write output")
+		return
 	}
 }
 
--- main.go
+++ main.go
@@ -3,35 +3,42 @@ package main
 import (
 	"github.com/awslabs/aws-lambda-go-api-proxy/gorillamux"
 	"github.com/gorilla/mux"
-	"io/ioutil"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
+var muxLambda *gorillamux.GorillaMuxAdapter
+
 // Handler is executed by AWS Lambda in the main function. Once the request
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// default response
-	index, err := ioutil.ReadFile("public/index.html")
-	if err != nil {
-		return events.APIGatewayProxyResponse{}, err
-	}
-
-	return events.APIGatewayProxyResponse{
-		StatusCode: 200,
-		Body:       string(index),
-		Headers: map[string]string{
-			"Content-Type": "text/html",
-		},
-	}, nil
+	//index, err := ioutil.ReadFile("public/index.html")
+	//if err != nil {
+	//	return events.APIGatewayProxyResponse{}, err
+	//}
+	//
+	//return events.APIGatewayProxyResponse{
+	//	StatusCode: 200,
+	//	Body:       string(index),
+	//	Headers: map[string]string{
+	//		"Content-Type": "text/html",
+	//	},
+	//}, nil
+	return muxLambda.Proxy(request)
 }
 
-func main() {
+func init() {
+	log.Println("Mux cold start")
 	router := mux.NewRouter().StrictSlash(false)
 	router.HandleFunc("/test", testHandler)
 	router.HandleFunc("/", handler)
 
-	proxy := gorillamux.New(router)
-	lambda.Start(proxy)
+	muxLambda = gorillamux.New(router)
+}
+
+func main() {
+	lambda.Start(Handler)
 }
