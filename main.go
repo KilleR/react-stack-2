a8580983ab58a40550a7412096cf708647f8aaa3
Richard Kille <r.kille@teluxhd.com>
2019-02-19 11:19:23 +0000

    attempt 1 at adding CORS headers for all requests

--- main.go
+++ main.go
@@ -14,7 +14,9 @@ var muxLambda *gorillamux.GorillaMuxAdapter
 // Handler is executed by AWS Lambda in the main function. Once the request
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	return muxLambda.Proxy(request)
+	proxyRes, err := muxLambda.Proxy(request)
+	proxyRes.Headers["Access-Control-Allow-Origin"] = "'*'"
+	return proxyRes, err
 }
 
 func init() {
