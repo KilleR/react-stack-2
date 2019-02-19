ed378d9e57de4ca765f00e79a547eee0878d0e6d
Richard Kille <r.kille@teluxhd.com>
2019-02-19 11:37:35 +0000

    works, setting up origin, headers and methods headers

--- main.go
+++ main.go
@@ -15,7 +15,9 @@ var muxLambda *gorillamux.GorillaMuxAdapter
 // is processed, it returns an Amazon API Gateway response object to AWS Lambda
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	proxyRes, err := muxLambda.Proxy(request)
-	proxyRes.Headers["Access-Control-Allow-Origin"] = "'*'"
+	proxyRes.Headers["Access-Control-Allow-Origin"] = "*"
+	proxyRes.Headers["Access-Control-Allow-Headers"] = "*"
+	proxyRes.Headers["Access-Control-Allow-Methods"] = "*"
 	return proxyRes, err
 }
 
